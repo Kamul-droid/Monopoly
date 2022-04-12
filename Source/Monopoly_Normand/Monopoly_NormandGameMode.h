c460f49bd2f0d4f7ec8194d09e4f8cd27bcfaee9
Corentin Hoareau <corentinhoareaufr@gmail.com>
2022-04-12 15:09:17 +0400

    push project to git, Basic menus and translation system

---
+++ Source/Monopoly_Normand/Monopoly_NormandGameMode.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Monopoly_NormandGameMode.generated.h"
+
+/** GameMode class to specify pawn and playercontroller */
+UCLASS(minimalapi)
+class AMonopoly_NormandGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMonopoly_NormandGameMode();
+};
+
+
+

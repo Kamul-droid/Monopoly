d2a47ee060d45e00de00c45e757964c989d58663
Corentin Hoareau <corentinhoareaufr@gmail.com>
2022-05-03 17:28:09 +0400

    clean restart on a blueprint template

--- Source/Monopoly_Normand/Monopoly_Normand.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Monopoly_Normand.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Monopoly_Normand, "Monopoly_Normand");
